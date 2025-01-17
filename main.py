1e1eb2e6491ee4dbbf99deb688d41d3d61681c6a
Felipe López <flh.1989@gmail.com>
2025-01-16 21:49:26 -0600

    tavily + tools agents example

--- main.py
+++ main.py
@@ -1,25 +1,55 @@
-from langchain_anthropic import ChatAnthropic
-from langchain_openai import ChatOpenAI
-from langchain_core.messages import HumanMessage, SystemMessage
 import logging
+from dotenv import load_dotenv
+
+from langchain.agents import create_openai_tools_agent
+from langchain_anthropic import ChatAnthropic
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+
 logging.getLogger("httpx").setLevel(logging.DEBUG)
 
+load_dotenv()  # Load environment variables from .env file
 
 
 def invoke_anthropic_model():
     model = ChatAnthropic(model="claude-3-5-sonnet-20240620")
     messages = [
         SystemMessage("Translate the following from English into Italian"),
-        HumanMessage("hi!")
+        HumanMessage("hi!"),
     ]
 
     return model.invoke(messages)
+
 
 def invoke_openai_model():
-    model = ChatOpenAI(model="gpt-4o-mini")
+    model = ChatOpenAI(model="gpt-4")
     messages = [
         SystemMessage("Translate the following from English into Italian"),
-        HumanMessage("hi!")
+        HumanMessage("hi!"),
     ]
 
     return model.invoke(messages)
+
+
+def invoke_openai_agent():
+    llm = ChatOpenAI(
+        model="gpt-4",
+        temperature=0.7,
+    )
+    tools = [TavilySearchResults(max_results=1)]
+
+    PROMPT = """You are a helpful AI assistant.
+
+    Human: {input}
+    Assistant: Let me help you with that. {agent_scratchpad}"""
+
+    prompt = ChatPromptTemplate.from_template(PROMPT)
+
+    agent = create_openai_tools_agent(llm, tools, prompt)
+    return agent.invoke({
+        "input": "What is the capital of France?",
+        "agent_scratchpad": "",
+        "intermediate_steps": [],
+    })
--- test_main.py
+++ test_main.py
@@ -1,18 +1,13 @@
 import pytest
-from main import invoke_anthropic_model, invoke_openai_model
+
+from main import invoke_anthropic_model, invoke_openai_agent, invoke_openai_model
 
 
 @pytest.fixture(scope="module")
 def vcr_config():
     return {
-        "filter_headers": [
-            "authorization",
-            "x-api-key"
-        ],
-        "filter_query_parameters": [
-            "api-key",
-            "access_token"
-        ]
+        "filter_headers": ["authorization", "x-api-key"],
+        "filter_query_parameters": ["api-key", "access_token"],
     }
 
 
@@ -26,3 +21,10 @@ def test_invoke_anthropic_model():
 def test_invoke_openai_model():
     result = invoke_openai_model()
     assert result.content == "Ciao!"
+
+
+@pytest.mark.vcr
+def test_invoke_openai_agent():
+    result = invoke_openai_agent()
+    assert result.return_values == {'output': 'The capital of France is Paris.'}
+
